299230f2c57e5d65c9537530096b7a284465ab1e
chrissyast <cjastles@gmail.com>
2020-11-30 22:43:04 +0000

    create language picker and header

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <LanguagePicker/>
+  </div>
+</template>
+
+<script>
+import LanguagePicker from "@/components/LanguagePicker";
+export default {
+  name: "Header",
+  components: {LanguagePicker},
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/LanguagePicker.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <select @change="changeLanguage($event)">
+    <option v-for="{code, name} in languages"
+            :value="code"
+            :selected="selectedLanguage===code">
+      {{name}}
+    </option>
+  </select>
+</div>
+</template>
+
+<script>
+export default {
+  name: "LanguagePicker",
+  methods: {
+   changeLanguage(event) {
+     this.$store.commit("changeLanguage", event.target.value)
+   }
+  },
+  data() {
+    return {
+      languages: [{code:"en-gb",name:"English"},{code:"es-es",name:"Español"},{code:"zh-cn",name:"简体中文"}]
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
