5a02c2db9fa2763e75a9a04859cc3d56f2b29796
Chris <cjastles@gmail.com>
2022-04-19 12:25:00 +0100

    unwanted plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~/plugins/axios',
+    // '~/plugins/axios',
     { src: "~/plugins/pwa-update.js", mode: "client" }
   ],
 
