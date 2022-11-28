d07fc66fd0d57e50dae6e3ffe69f8405c0004bde
chrissyast <cjastles@gmail.com>
2022-11-28 12:46:37 +0000

    implement card container

---
+++ components/CompactedCardContainer.vue
@@ -0,0 +1,103 @@
+<template>
+  <div :class="outerClass" style="width: 100%;">
+    <div id="parent-container" style="position:relative">
+      <div
+        v-for="(c, index) in $slots.default"
+        :id="`child-${index}`"
+        :key="`child-${index}`"
+        class="child-card"
+        :class="innerClass"
+        :style="`position: absolute; width: ${100 / perRow}%;`"
+      >
+        <VNodes :vnodes="c" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { debounce } from 'lodash'
+
+export default {
+  components: {
+    VNodes: {
+      functional: true,
+      render: (_, ctx) => ctx.props.vnodes
+    }
+  },
+  props: {
+    outerClass: {
+      type: String,
+      default: ''
+    },
+    innerClass: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      perRowSizes: {
+        xs: 1,
+        sm: 1,
+        md: 2,
+        lg: 2,
+        xl: 3
+      },
+      heightMap: {}
+    }
+  },
+  computed: {
+    perRow() {
+      return this.perRowSizes[this.$vuetify.breakpoint.name]
+    }
+  },
+  beforeMount() {
+    window.addEventListener('resize', this.debouncedRearrageCards)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.debouncedRearrageCards)
+  },
+  created() {
+    this.debouncedRearrageCards = debounce(this.rearrangeCards, 200, {
+      leading: false,
+      trailing: true
+    })
+  },
+  mounted() {
+    setTimeout(this.rearrangeCards, 500)
+  },
+  methods: {
+    translation(index) {
+      if (!this.perRow) { return }
+      const { heightMap } = this
+      const shortestColumn = Math.min(...Object.values(heightMap))
+      const x = parseInt(Object.keys(heightMap).find(k => heightMap[k] === shortestColumn))
+      const element = document.getElementById(`child-${index}`)
+      const y = heightMap[x]
+      heightMap[x] = heightMap[x] + element.offsetHeight
+      return `translate(${x * 100}%, ${y}px)`
+    },
+    rearrangeCards() {
+      this.heightMap = {}
+      for (let i = 0; i < this.perRow; i++) {
+        this.heightMap[i] = 0
+      }
+      for (let i = 0; i < this.$slots.default.length; i++) {
+        const element = document.getElementById(`child-${i}`)
+        element.style.transform = (`${this.translation(i)}`)
+      }
+      const parent = document.getElementById('parent-container')
+      const heightOfFirstCol = this.heightMap[0]
+      parent.style.height = `${heightOfFirstCol + 20}px`
+    },
+    debouncedRearrageCards() {}
+  }
+}
+</script>
+
+<style>
+  .child-card {
+    transition: transform 180ms ease-in-out;
+  }
+</style>
--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,47 +1,45 @@
 <template>
-  <v-col cols="12" sm="4">
-    <v-skeleton-loader v-if="!techsLoaded" type="image" />
-    <v-card v-else light>
-      <v-card-title>
-        <v-container class="d-flex">
-          <v-row align="center">
-            {{$prismic.asText(project.title)}}
-            <v-tooltip bottom v-for="(l, i) in techLogos" :key="`logo-${i}`">
-              <template v-slot:activator="{ on, attrs }">
-                <img height="20" width="20" contain :src="l.url" style="margin-left:2px; margin-right:2px" v-bind="attrs"
-          v-on="on" />
-              </template>
-              <span>{{l.tooltip}}</span>
-            </v-tooltip>
-            
-          </v-row>
-        </v-container>
-      </v-card-title>
-      <v-card-subtitle>{{$prismic.asText(project.subtitle)}}</v-card-subtitle>
-      <v-card-text>{{$prismic.asText(project.more_detail)}}</v-card-text>
-      <v-img v-if="project.demo_gif.url" :src="project.demo_gif.url" height="250" contain/>
-      <v-card-actions class="px-4 py-0">
-        <v-container class="d-flex">
-          <v-row align="center">
-            <v-col cols="6">
-              <v-row class="my-0" justify="start">
-                <a :href="project.github.url">
-                  <v-img v-if="project.github.url" src="assets/GitHub-Mark-32px.png" max-height="20px" max-width="20px" />
-                </a>
-              </v-row>
-            </v-col>
-            <v-col cols="6">
-              <v-row class="my-0" justify="end">
-                <link-button v-if="project.live_demo.url" external :to="project.live_demo.url" :label="translateByKey('checkItOut', storeLanguage)"></link-button>
-              </v-row>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-actions>
-      
-      <!-- TODO alignment and hover text -->
-    </v-card>
-  </v-col>
+  <v-skeleton-loader v-if="!techsLoaded" type="image" />
+  <v-card v-else light>
+    <v-card-title>
+      <v-container class="d-flex">
+        <v-row align="center">
+          {{$prismic.asText(project.title)}}
+          <v-tooltip bottom v-for="(l, i) in techLogos" :key="`logo-${i}`">
+            <template v-slot:activator="{ on, attrs }">
+              <img height="20" width="20" contain :src="l.url" style="margin-left:2px; margin-right:2px" v-bind="attrs"
+        v-on="on" />
+            </template>
+            <span>{{l.tooltip}}</span>
+          </v-tooltip>
+          
+        </v-row>
+      </v-container>
+    </v-card-title>
+    <v-card-subtitle>{{$prismic.asText(project.subtitle)}}</v-card-subtitle>
+    <v-card-text>{{$prismic.asText(project.more_detail)}}</v-card-text>
+    <v-img v-if="project.demo_gif.url" :src="project.demo_gif.url" height="250" contain/>
+    <v-card-actions class="px-4 py-0">
+      <v-container class="d-flex">
+        <v-row align="center">
+          <v-col cols="6">
+            <v-row class="my-0" justify="start">
+              <a :href="project.github.url">
+                <v-img v-if="project.github.url" src="assets/GitHub-Mark-32px.png" max-height="20px" max-width="20px" />
+              </a>
+            </v-row>
+          </v-col>
+          <v-col cols="6">
+            <v-row class="my-0" justify="end">
+              <link-button v-if="project.live_demo.url" external :to="project.live_demo.url" :label="translateByKey('checkItOut', storeLanguage)"></link-button>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-actions>
+    
+    <!-- TODO alignment and hover text -->
+  </v-card>
 </template>
 
 <script>
